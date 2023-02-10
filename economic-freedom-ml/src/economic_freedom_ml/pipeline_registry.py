35324c3190184d15c9ef5ec0c379bf00c94ee25a
camilatech <camila.bosa@indicium.tech>
2023-02-10 17:46:45 -0300

    adding feature_engineering_pipeline and its nodes

--- economic-freedom-ml/src/economic_freedom_ml/pipeline_registry.py
+++ economic-freedom-ml/src/economic_freedom_ml/pipeline_registry.py
@@ -3,6 +3,7 @@ from typing import Dict
 
 from kedro.pipeline import Pipeline, pipeline
 from economic_freedom_ml.pipelines import data_processing as dp
+from economic_freedom_ml.pipelines import feature_engineering as fe
 
 def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -11,7 +12,9 @@ def register_pipelines() -> Dict[str, Pipeline]:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
     data_processing_pipeline = dp.create_pipeline()
+    feature_engineering_pipeline = fe.create_pipeline()
 
     return {
-        "dp": data_processing_pipeline
+        "dp": data_processing_pipeline,
+        "fe": feature_engineering_pipeline
     }
