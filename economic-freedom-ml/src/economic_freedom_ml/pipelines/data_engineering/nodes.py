8c4d330662ac7bb312d082e4b71b41000a16c32f
camilatech <camila.bosa@indicium.tech>
2023-02-10 17:01:21 -0300

    creating data engineering pipeline

---
+++ economic-freedom-ml/src/economic_freedom_ml/pipelines/data_engineering/nodes.py
@@ -0,0 +1,12 @@
+"""
+This is a boilerplate pipeline 'data_science'
+generated using Kedro 0.18.1
+"""
+import pandas as pd
+
+def creating_features (preprocess_data, preprocess_world_data):
+    """ Fits a model using the training dataset
+    """
+
+
+    return linear_regression
