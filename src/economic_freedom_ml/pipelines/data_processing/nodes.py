b609c21718c178af6afa405dc015ec5932c8a61d
camilatech <camila.bosa@indicium.tech>
2023-03-03 14:38:14 -0300

    treating null values

--- src/economic_freedom_ml/pipelines/data_processing/nodes.py
+++ src/economic_freedom_ml/pipelines/data_processing/nodes.py
@@ -13,20 +13,21 @@ def preprocess_raw_data (heritage_foundation_database: pd.DataFrame) -> pd.DataF
         Preprocessed data with:
            null rows deleted, 
            replace 'The Bahamas' for 'Bahamas' to match the data,
-           rename columns.
+           rename columns,
+           substitute space for underline and lower case.
     """     
     pp_data = heritage_foundation_database.dropna(axis=0, subset=["Overall Score"])
-    pp_name = pp_data.replace('The Bahamas','Bahamas',regex=True)
-    preprocess_data = pp_name.rename(columns={
+    pp_replace_name = pp_data.replace('The Bahamas','Bahamas',regex=True)
+    pp_drop_columns = pp_replace_name.drop(axis=1, columns=["Judicial Effectiveness", "Fiscal Health"])
+    pp_na_fill = pp_drop_columns.fillna(value={"Labor Freedom": 0})
+    preprocess_data = pp_na_fill.rename(columns={
         "Name": "country",
         "Index Year": "year",
         "Overall Score": "score",
         "Property Rights": "property_right",
         "Government Integrity": "government_integrity",
-        "Judicial Effectiveness": "judicial_effectiveness",
         "Tax Burden": "tax_burden",
         "Government Spending": "government_spending",
-        "Fiscal Health": "fiscal_health",
         "Business Freedom": "business_freedom",
         "Labor Freedom": "labor_freedom",
         "Monetary Freedom": "monetary_freedom",
@@ -34,6 +35,7 @@ def preprocess_raw_data (heritage_foundation_database: pd.DataFrame) -> pd.DataF
         "Investment Freedom": "investment_freedom",
         "Financial Freedom": "financial_freedom"
         })
+    preprocess_data['country'] = preprocess_data['country'].replace(' ','_',regex=True).str.lower()
     return preprocess_data
 
 
@@ -44,18 +46,12 @@ def preprocess_world (world_database: pd.DataFrame) -> pd.DataFrame:
         world_data: Raw data.
     Returns:
         Preprocessed data with:
-           selecting only the necessary columns
-           creating dummies and renaming columns.
+           selecting only the necessary columns.
     """     
-    selecting_columns = world_database[["country", "region"]]
-    making_dummies = pd.get_dummies(selecting_columns, columns=['region'])
-    preprocess_world_data = making_dummies.rename(columns={
-        "region_Africa":"africa",
-        "region_Asia":"asia",
-        "region_Europe":"europe",
-        "region_North America":"north_america",
-        "region_Oceania":"oceania",
-        "region_South America":"south_america"
-        })
+    preprocess_world_data = world_database[["country", "region"]]
+
+    preprocess_world_data['region'] = preprocess_world_data['region'].replace(' ','_',regex=True).str.lower()
+    
+    preprocess_world_data['country'] = preprocess_world_data['country'].replace(' ','_',regex=True).str.lower()
     
     return preprocess_world_data
